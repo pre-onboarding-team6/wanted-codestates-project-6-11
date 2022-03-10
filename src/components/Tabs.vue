42286e21ea324fcd64b51a043f6be35fd279d41c
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2022-03-10 23:16:07 +0900

    Feat: Tab 구현

---
+++ src/components/Tabs.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div class="tab-container">
+      <Tab
+        :key="index"
+        v-for="(tab, index) in tabs"
+        :tabOrder="tab.order"
+        :tabName="tab.content"
+        v-bind="tab"
+        v-model="currentTab"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Tab from './tabs/Tab.vue';
+
+export default {
+  name: 'TabsComponent',
+  components: {
+    Tab,
+  },
+  props: {
+    tabName: String,
+  },
+  data() {
+    return {
+      currentTab: 'tab1', // tab 구분 지표
+      tabs: [
+        { order: 'tab1', content: '모두' },
+        { order: 'tab2', content: '본인' },
+        { order: 'tab3', content: '회사' },
+      ],
+    };
+  },
+  computed: {
+    current() {
+      return this.tabs.find((el) => el.order === this.currentTab) || {};
+    },
+  },
+};
+</script>
+
+<style scoped>
+.tab-container {
+  background-color: #f2f2f2;
+  color: black;
+  width: 100%;
+  display: flex;
+}
+</style>
---
+++ src/components/tabs/Tab.vue
@@ -0,0 +1,61 @@
+<template>
+  <button :class="[checkFocus, 'tab']" @click="$emit('input', tabOrder)">
+    <Icon
+      :isDouble="tabOrder === 'tab1' ? true : false"
+      :isCompany="tabOrder === 'tab3' ? true : false"
+    />
+    <span :class="[checkFocus, 'type']">{{ tabName }}</span>
+  </button>
+</template>
+
+<script>
+import Icon from './Icon.vue';
+
+export default {
+  name: 'TabComponent',
+  components: {
+    Icon,
+  },
+  props: {
+    tabOrder: String,
+    tabName: String,
+    value: String,
+  },
+  computed: {
+    checkFocus() {
+      return this.tabOrder === this.value ? 'focus' : '';
+    },
+  },
+};
+</script>
+
+<style scoped>
+.tab {
+  display: flex;
+  background-color: #f2f2f2;
+  margin: 1px;
+  border-radius: 3px;
+  padding: 15px 30px;
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-content: center;
+  border: none;
+  cursor: pointer;
+}
+.tab:hover {
+  background-color: #fff;
+}
+.tab.focus {
+  background-color: #fff;
+}
+
+.type {
+  font-size: 12px;
+  color: #727272;
+  margin-left: 3px;
+}
+.type.focus {
+  color: black;
+}
+</style>
